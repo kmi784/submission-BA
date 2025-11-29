2f0e3e02236492cdf5e3940cf3833f5dd629d20c
kmitte <kaspar.mitte@icloud.com>
2025-11-29 01:13:19 +0100

    set up master make file

--- src/equi/mean.cpp
+++ src/equi/mean.cpp
@@ -16,86 +16,95 @@ namespace fs = std::filesystem;
 std::ifstream fin;
 std::ofstream fout;
 
-
 double L;
 
 
-class Quantities{
-    private :
-        struct Observables{
-            double e1  = 0.0, e2  = 0.0;
-            double mF1 = 0.0, mF2 = 0.0;
-            double mS1 = 0.0, mS2 = 0.0;
+class Quantities
+{
+private :
+    struct Observables {
+        double e1  = 0.0, e2  = 0.0;
+        double mF1 = 0.0, mF2 = 0.0;
+        double mS1 = 0.0, mS2 = 0.0;
 
 
-            void operator+=(Observables a){
-                e1  += a.e1;  e2  += a.e2;
-                mF1 += a.mF1; mF2 += a.mF2;
-                mS1 += a.mS1; mS2 += a.mS2;
+        void operator+=(Observables a)
+        {
+            e1  += a.e1;  e2  += a.e2;
+            mF1 += a.mF1; mF2 += a.mF2;
+            mS1 += a.mS1; mS2 += a.mS2;
 
-            }
+        }
 
-            void operator*=(double a){
-                e1  *= a; e2  *= a;
-                mF1 *= a; mF2 *= a;
-                mS1 *= a; mS2 *= a;
-            }
+        void operator*=(double a)
+        {
+            e1  *= a; e2  *= a;
+            mF1 *= a; mF2 *= a;
+            mS1 *= a; mS2 *= a;
+        }
 
-            void operator/=(double a){
-                e1  /= a; e2  /= a;
-                mF1 /= a; mF2 /= a;
-                mS1 /= a; mS2 /= a; 
-            }
+        void operator/=(double a)
+        {
+            e1  /= a; e2  /= a;
+            mF1 /= a; mF2 /= a;
+            mS1 /= a; mS2 /= a;
+        }
 
-            Observables operator*(Observables a){
-                return{e1  * a.e1,  e2  * a.e2,
-                       mF1 * a.mF1, mF2 * a.mF2, 
-                       mS1 * a.mS1, mS2 * a.mS2};
-            }
+        Observables operator*(Observables a)
+        {
+            return{e1  * a.e1,  e2  * a.e2,
+                   mF1 * a.mF1, mF2 * a.mF2,
+                   mS1 * a.mS1, mS2 * a.mS2};
+        }
 
-            Observables operator-(Observables a){
-                return{e1  - a.e1,  e2  - a.e2,
-                       mF1 - a.mF1, mF2 - a.mF2,
-                       mS1 - a.mS1, mS2 - a.mS2};
-            }
-        };
+        Observables operator-(Observables a)
+        {
+            return{e1  - a.e1,  e2  - a.e2,
+                   mF1 - a.mF1, mF2 - a.mF2,
+                   mS1 - a.mS1, mS2 - a.mS2};
+        }
+    };
 
-        vector<Observables> Timeseries;
-        unsigned int N = 0, nJ = 0, NJ = 0;
+    vector<Observables> Timeseries;
+    unsigned int N = 0, nJ = 0, NJ = 0;
 
-    public :
-        Quantities(string);
-        string jackknife(double);
+public :
+    Quantities(string);
+    string jackknife(double);
 };
 
 
+int main(int argc, char* argv[])
+{
+    /***********************************************************************************
+        mean Parameter Setup
+        --------------------------
+        Expected cli-arguments:
+            argv[1]: 'relative path + name' of target-folder
+            argv[2]: 'relative path + name' of output-file
+            argv[3]: Lattice length
 
+    ***********************************************************************************/
 
-
-int main(int argc, char* argv[]){
-
-    std::string target = argv[1];                                       // 'relative path + name' of target-folder
-    std::string output = argv[2];                                       // 'relative path + name' of output-file
-    L = std::stod(target.substr(target.find("L")+1));                   // lattice length
-
-
+    std::string target = argv[1];
+    std::string output = argv[2];
+    L = std::stod(target.substr(target.find("L") + 1));
 
     std::set<fs::path> chronological;
-    for(const auto& entry : fs::directory_iterator(target)){
+    for (const auto& entry : fs::directory_iterator(target)) {
         chronological.insert(entry.path());
     }
 
     fout.open(output);
-        for(string folder : chronological){
-            if(folder.find("T") != string::npos){
-                double T = std::stod(folder.substr(folder.find("T")+1))/1000;
-                Quantities simu_point(folder);
-                fout << simu_point.jackknife(T) << "\n";
-            }
+    for (string folder : chronological) {
+        if (folder.find("T") != string::npos) {
+            double T = std::stod(folder.substr(folder.find("T") + 1)) / 1000;
+            Quantities simu_point(folder);
+            fout << simu_point.jackknife(T) << "\n";
         }
+    }
     fout.close();
 
-        
     return 0;
 }
 
@@ -103,26 +112,27 @@ int main(int argc, char* argv[]){
 
 
 
-Quantities::Quantities(string directory){
-    for(const auto&  entry : fs::directory_iterator(directory)){
+Quantities::Quantities(string directory)
+{
+    for (const auto&  entry : fs::directory_iterator(directory)) {
         string file = fs::path(entry);
-        
-        
-        if(file.find("block") != string::npos){
+
+
+        if (file.find("block") != string::npos) {
             Observables input;
 
             fin.open(file);
-                double E; unsigned int C; 
-                double MF1, MS1, MF2, MS2; 
-                nJ = 0; 
-                while(fin >> E >> C >> MF1 >> MS1 >> MF2 >> MS2){
+            double E; unsigned int C;
+            double MF1, MS1, MF2, MS2;
+            nJ = 0;
+            while (fin >> E >> C >> MF1 >> MS1 >> MF2 >> MS2) {
 
-                    input.e1  += E*(double)C/(L*L); input.e2  += E*E*(double)C/(L*L*L*L);
-                    input.mF1 += MF1;               input.mF2 += MF2;    
-                    input.mS1 += MS1;               input.mS2 += MS2;   
-                      
-                    nJ += C;
-                }
+                input.e1  += E * (double)C / (L * L); input.e2  += E * E * (double)C / (L * L * L * L);
+                input.mF1 += MF1;               input.mF2 += MF2;
+                input.mS1 += MS1;               input.mS2 += MS2;
+
+                nJ += C;
+            }
             fin.close();
 
             ++NJ;
@@ -130,41 +140,42 @@ Quantities::Quantities(string directory){
         }
     }
 
-    N = NJ*nJ;
+    N = NJ * nJ;
 }
 
 
-
-
-
-string Quantities::jackknife(double T){
+string Quantities::jackknife(double T)
+{
 
     Observables mean;
-    for(Observables o : Timeseries){mean += o;}
+    for (Observables o : Timeseries) {mean += o;}
     mean /= N;
 
-    Observables observable{mean.e1,  (L*L)*(mean.e2  - mean.e1*mean.e1)/(T*T),
-                           mean.mF1, (L*L)*(mean.mF2 - mean.mF1*mean.mF1)/T,
-                           mean.mS1, (L*L)*(mean.mS2 - mean.mS1*mean.mS1)/T};
+    Observables observable{mean.e1, (L * L)*(mean.e2  - mean.e1 * mean.e1) / (T * T),
+                           mean.mF1, (L * L)*(mean.mF2 - mean.mF1 * mean.mF1) / T,
+                           mean.mS1, (L * L)*(mean.mS2 - mean.mS1 * mean.mS1) / T};
     mean *= N;
 
     Observables sigma;
-    for( Observables o : Timeseries){
-        o = mean-o;
-        o /= (double)(nJ*(NJ-1));
+    for (Observables o : Timeseries) {
+        o = mean - o;
+        o /= (double)(nJ * (NJ - 1));
 
-        o.e2  = (L*L)*(o.e2  - o.e1*o.e1)/(T*T);
-        o.mF2 = (L*L)*(o.mF2 - o.mF1*o.mF1)/T; 
-        o.mS2 = (L*L)*(o.mS2 - o.mS1*o.mS1)/T; 
+        o.e2  = (L * L) * (o.e2  - o.e1 * o.e1) / (T * T);
+        o.mF2 = (L * L) * (o.mF2 - o.mF1 * o.mF1) / T;
+        o.mS2 = (L * L) * (o.mS2 - o.mS1 * o.mS1) / T;
 
-        sigma += (o-observable)*(o-observable);
+        sigma += (o - observable) * (o - observable);
     }
-    sigma *= (double)(NJ-1)/NJ;
+    sigma *= (double)(NJ - 1) / NJ;
 
     std::stringstream out;
-    out << T << "\t" << observable.e1  << "\t" << sqrt(sigma.e1)  << "\t" << observable.e2  << "\t" << sqrt(sigma.e2)  << "\t"
-                     << observable.mF1 << "\t" << sqrt(sigma.mF1) << "\t" << observable.mF2 << "\t" << sqrt(sigma.mF2) << "\t"
-                     << observable.mS1 << "\t" << sqrt(sigma.mS1) << "\t" << observable.mS2 << "\t" << sqrt(sigma.mS2) << "\t";
+    out << T << "\t" << observable.e1  << "\t" << sqrt(sigma.e1)  << "\t" << observable.e2
+        << "\t" << sqrt(sigma.e2)  << "\t"
+        << observable.mF1 << "\t" << sqrt(sigma.mF1) << "\t" << observable.mF2 << "\t" << sqrt(
+            sigma.mF2) << "\t"
+        << observable.mS1 << "\t" << sqrt(sigma.mS1) << "\t" << observable.mS2 << "\t" << sqrt(
+            sigma.mS2) << "\t";
 
     return out.str();
 }
--- src/equi/peak.cpp
+++ src/equi/peak.cpp
@@ -14,197 +14,218 @@ using std::pair;
 using std::string;
 namespace fs = std::filesystem;
 
-/**** Double precision ********/ const double epsilon = 1e-5;
+// double precision
+const double epsilon = 1e-5;
 
 std::ifstream fin;
 std::ofstream fout;
 
-class Multihistogram{
-    private :                                       //        T[0]      T[1]    ...  T[M]   
-        vector<double> Temperature;                 // E[0] | H[0][0]  H[0][1]  ...  H[0][M] 
-        vector<double> Energy;                      // E[1] | H[1][0]  H[1][1]  ...  H[1][M] 
-        vector<vector<unsigned int>> Counts;        // ...  | ...      ...      ...  ...     
-                                                    // E[N] | H[N][0]  H[N][1]  ...  H[N][M] 
-        
-        
-    public :
-        unsigned int Nener, Ntemp;
+class Multihistogram
+{
+    /***********************************************************************************
+        Represents density of states. It is comuted by loading Temperature dependent
+        histogramm 'entire.txt' or 'block{Number of block}.txt' in 'T{1000*temperature}/'
+        in target folder L{lattice length}-{Initial state}/
 
-        Multihistogram(string, string);     // load Temperature dependent histogramm 'entire.txt' or 'block_.txt' 
-                                            // in 'T__/' in target folder L__-_/
+        Data structure
+        --------------
+               T[0]     T[1]     ...  T[M]
+        E[0] | H[0][0]  H[0][1]  ...  H[0][M] | <f(m)>(E[0])
+        E[1] | H[1][0]  H[1][1]  ...  H[1][M] | <f(m)>(E[1])
+        ...  | ...      ...      ...  ...     | ...
+        E[N] | H[N][0]  H[N][1]  ...  H[N][M] | <f(m)>(E[M])
 
-        static double logplus(double a, double b){
-            return a > b ? a+log(1+exp(b-a)) : b+log(1+exp(a-b));
-        }
+        Density of states (dos)
+        -----------------------
+            - engery dependent dos is represented by an array of pair<floar, float>
 
-        pair<double,double>* compute_logDOS(double=epsilon);    // computes logarithm density of states
+    ***********************************************************************************/
+private :
+    vector<double> Temperature;
+    vector<double> Energy;
+    vector<vector<unsigned int >> Counts;
 
-        Multihistogram operator-(const Multihistogram&)const;
+public :
+    unsigned int Nener, Ntemp;
+    Multihistogram(string, string);
+    static double logplus(double a, double b)
+    {
+        return a > b ? a + log(1 + exp(b - a)) : b + log(1 + exp(a - b));
+    }
+
+    // computes logarithm density of states
+    pair<double, double> *compute_logDOS(double = epsilon);
+
+    Multihistogram operator-(const Multihistogram &)const;
 };
 
 
 
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[])
+{
+    /***********************************************************************************
+        peak Parameter Setup
+        --------------------------
+        Expected cli-arguments:
+            argv[1]: 'relative path + name' of target-folder
+            argv[2]: 'relative path + name' of output-file
+            argv[3]: Number of Jackknife blocks
+            argv[3]: Minimal temperature
+            argv[4]: Maximal temperature
+            argv[5]: Number of output-datapoints
 
-    string target = argv[1];                                            // 'relative path + name' of target-folder
-    string output = argv[2];                                            // 'relative path + name' of output-file
-    unsigned int Nblock = std::stoul(argv[3]);                          // number of Jackknife blocks
-    double T1 = std::stod(argv[4]); double T2 = std::stod(argv[5]);     // temperature-range of output-data
-    unsigned int Ndata = std::stoul(argv[6]);                           // number of output-datapoints
+    ***********************************************************************************/
 
-
+    string target = argv[1];
+    string output = argv[2];
+    unsigned int Nblock = std::stoul(argv[3]);
+    double T1 = std::stod(argv[4]);
+    double T2 = std::stod(argv[5]);
+    unsigned int Ndata = std::stoul(argv[6]);
 
     Multihistogram data_entire(target, "entire.txt");
-    int L = std::stoi(target.substr(target.find("L")+1));
+    int L = std::stoi(target.substr(target.find("L") + 1));
 
-
-
-    //******************** Data examination ********************//
-    double Tmax = 0.0, Cmax = 0.0; 
-    pair<double,double>* peaks = new pair<double,double>[Nblock];
-    for(unsigned int i = 1; i <= Nblock; ++i){
+    // data examination
+    double Tmax = 0.0, Cmax = 0.0;
+    pair<double, double> *peaks = new pair<double, double>[Nblock];
+    for (unsigned int i = 1; i <= Nblock; ++i) {
 
         Multihistogram data_block(target, "block" + std::to_string(i) + ".txt");
-        data_block = data_entire-data_block;
+        data_block = data_entire - data_block;
 
-        pair<double,double>* logOmega = data_block.compute_logDOS(1e-10);
-        
+        pair<double, double> *logOmega = data_block.compute_logDOS(1e-10);
 
-        //***** Heat-data-Determination *****/
-        vector<pair<double,double>> heat;
-        for(unsigned int j = 0; j < Ndata; ++j){
-            double T = T1 + j*(T2-T1)/Ndata;
-            double logZ = logOmega[0].second - logOmega[0].first/T;
+        // determination of heat
+        vector<pair<double, double >> heat;
+        for(unsigned int j = 0; j < Ndata; ++j) {
+            double T = T1 + j*(T2 - T1) / Ndata;
+            double logZ = logOmega[0].second - logOmega[0].first / T;
 
-            for(unsigned int E = 1; E < data_block.Nener; ++E){ 
-                logZ = Multihistogram::logplus(logZ, logOmega[E].second-logOmega[E].first/T); 
+            for(unsigned int E = 1; E < data_block.Nener; ++E) {
+                logZ = Multihistogram::logplus(logZ, logOmega[E].second - logOmega[E].first / T);
             }
 
             double E1 = 0.0; double E2 = 0.0;
-            for(unsigned int E = 0; E < data_block.Nener; ++E){
-                E1 += logOmega[E].first * exp(logOmega[E].second-logOmega[E].first/T-logZ);
-                E2 += logOmega[E].first*logOmega[E].first * exp(logOmega[E].second-logOmega[E].first/T-logZ);
+            for(unsigned int E = 0; E < data_block.Nener; ++E) {
+                E1 += logOmega[E].first * exp(logOmega[E].second - logOmega[E].first / T - logZ);
+                E2 += logOmega[E].first*logOmega[E].first * exp(logOmega[E].second - logOmega[E].first / T - logZ);
             }
 
-            heat.push_back({T, (E2-E1*E1)/(T*T*L*L)});
+            heat.push_back({T, (E2 - E1*E1) / (T*T*L*L)});
         }
 
         delete[] logOmega;
 
-
-        //***** Maximum-Determination  *****/
-        pair<double,double> maximum = heat[0];
-        for(pair<double,double> point : heat){
-            if(maximum.second < point.second){
+        // maximum determination
+        pair<double, double> maximum = heat[0];
+        for (pair<double, double> point : heat) {
+            if (maximum.second < point.second) {
                 maximum = point;
             }
         }
 
-        peaks[i-1] = maximum;
+        peaks[i - 1] = maximum;
         Tmax += maximum.first; Cmax += maximum.second;
-        std::cout << "Tmax = " << maximum.first  << "\t" 
+        std::cout << "Tmax = " << maximum.first  << "\t"
                   << "Cmax = " << maximum.second << "\n";
     }
     Tmax /= Nblock; Cmax /= Nblock;
 
 
-    pair<double,double> sigma{0.0, 0.0};
-    for(unsigned int i = 0; i < Nblock; ++i){
-        sigma.first  += (Tmax-peaks[i].first)*(Tmax-peaks[i].first);
-        sigma.second += (Cmax-peaks[i].second)*(Cmax-peaks[i].second);
+    pair<double, double> sigma{0.0, 0.0};
+    for (unsigned int i = 0; i < Nblock; ++i) {
+        sigma.first  += (Tmax - peaks[i].first) * (Tmax - peaks[i].first);
+        sigma.second += (Cmax - peaks[i].second) * (Cmax - peaks[i].second);
     }
-    sigma.first  = sqrt((Nblock-1)*sigma.first/Nblock); 
-    sigma.second = sqrt((Nblock-1)*sigma.second/Nblock); 
+    sigma.first  = sqrt((Nblock - 1) * sigma.first / Nblock);
+    sigma.second = sqrt((Nblock - 1) * sigma.second / Nblock);
 
     delete[] peaks;
 
-
-
-    //***** Storeing data *****/
     std::stringstream ss;
     ss << Tmax << "\t" << sigma.first << "\t" << Cmax << "\t" << sigma.second;
     pair<int, string> entry_new{L, ss.str()};
-    
 
-    vector<pair<int,string>> data;
-    fin.open(output);{
-        string line; pair<int,string> entry; bool append = true;
-        while(getline(fin, line)){
-            ss.clear(); ss.str(line); 
+    vector<pair<int, string >> data;
+    fin.open(output); {
+        string line; pair<int, string> entry; bool append = true;
+        while (getline(fin, line)) {
+            ss.clear(); ss.str(line);
             ss >> entry.first; ss.ignore(); getline(ss, entry.second);
-            if(L==entry.first){
+            if (L == entry.first) {
                 data.push_back(entry_new);
                 append = false;
-            }else{
+            } else {
                 data.push_back(entry);
             }
         }
 
-        if(append){
+        if (append) {
             data.push_back(entry_new);
-            std::sort(data.begin(),data.end());
+            std::sort(data.begin(), data.end());
         }
-    }fin.close();
+    } fin.close();
 
 
     fout.open(output);
-        for(pair<int,string> entry : data){
-            fout << entry.first << "\t" << entry.second << "\n";
-        }
+    for (pair<int, string> entry : data) {
+        fout << entry.first << "\t" << entry.second << "\n";
+    }
     fout.close();
-    
+
     return 0;
 }
 
 
-
-Multihistogram::Multihistogram(string directory, string file){
+Multihistogram::Multihistogram(string directory, string file)
+{
 
     std::set<fs::path> chronological;
-    for(const auto& entry : fs::directory_iterator(directory)){
+    for (const auto& entry : fs::directory_iterator(directory)) {
         chronological.insert(entry.path());
     }
 
-    for(string folder : chronological){
+    for (string folder : chronological) {
 
-        if(folder.find("T") != string::npos){
-            Temperature.push_back(std::stod(folder.substr(folder.find("T")+1))/1000);
-            for(vector<unsigned int>& h : Counts){h.push_back(0);}
+        if (folder.find("T") != string::npos) {
+            Temperature.push_back(std::stod(folder.substr(folder.find("T") + 1)) / 1000);
+            for (vector<unsigned int> &h : Counts) {h.push_back(0);}
 
             fin.open(folder + "/" + file);
-                double E; unsigned int C;
+            double E; unsigned int C;
 
-                if(Energy.size()==0){
-                    while(fin >> E >> C){
-                        fin.ignore(200, '\n');
+            if (Energy.size() == 0) {
+                while (fin >> E >> C) {
+                    fin.ignore(200, '\n');
 
+                    Energy.push_back(E);
+
+                    vector<unsigned int> help(Temperature.size(), 0); Counts.push_back(help);
+                    Counts[Energy.size() - 1][Temperature.size() - 1] = C;
+                }
+            } else {
+                while (fin >> E >> C) {
+                    fin.ignore(200, '\n');
+
+                    bool append = true;
+                    for (unsigned int i = 0; i < Energy.size(); ++i) {
+                        if (abs(Energy[i] - E) < epsilon) {
+                            Counts[i][Temperature.size() - 1] = C;
+
+                            append = false; break;
+                        }
+                    }
+
+                    if (append) {
                         Energy.push_back(E);
 
-                        vector<unsigned int> help(Temperature.size(),0); Counts.push_back(help);
-                        Counts[Energy.size()-1][Temperature.size()-1] = C;
-                    }
-                }else{
-                    while(fin >> E >> C){
-                        fin.ignore(200, '\n');
-
-                        bool append = true;
-                        for(unsigned int i = 0; i < Energy.size(); ++i){
-                            if(abs(Energy[i]-E)<epsilon){
-                                Counts[i][Temperature.size()-1] = C;
-
-                                append = false; break;
-                            }
-                        }
-
-                        if(append){
-                            Energy.push_back(E);
-
-                            vector<unsigned int> help(Temperature.size(),0); Counts.push_back(help);
-                            Counts[Energy.size()-1][Temperature.size()-1] = C;
-                        }
+                        vector<unsigned int> help(Temperature.size(), 0); Counts.push_back(help);
+                        Counts[Energy.size() - 1][Temperature.size() - 1] = C;
                     }
                 }
+            }
             fin.close();
         }
     }
@@ -214,61 +235,57 @@ Multihistogram::Multihistogram(string directory, string file){
 }
 
 
+pair<double, double> *Multihistogram::compute_logDOS(double precision)
+{
 
+    pair<double, double> *logOmega = new pair<double, double>[Nener]; // output-pointer
+    pair<double, double> *logZ = new
+    pair<double, double>[Ntemp]; // logarithm of partionfunction
+    double *logN = new double[Ntemp]; // logarithm of normalization constant of the histogram
 
-
-pair<double,double>* Multihistogram::compute_logDOS(double precision){
-
-    pair<double,double>* logOmega = new pair<double,double>[Nener];     // output-pointer
-    pair<double,double>* logZ = new pair<double,double>[Ntemp];         // logarithm of partionfunction
-    double* logN = new double[Ntemp];                                   // logarithm of normalization constant of the histogram
-
-
-    for(unsigned int T = 0; T < Ntemp; ++T){
+    for (unsigned int T = 0; T < Ntemp; ++T) {
         logZ[T] = {100.0, 0.0};
         logN[T] = 0.0;
-        for(unsigned int E = 0; E < Nener; ++E){
+        for (unsigned int E = 0; E < Nener; ++E) {
             logN[T] += Counts[E][T];
         }
         logN[T] = log(logN[T]);
     }
 
-
-    unsigned int accuracy; do{
-
-        //********** Computing log(density of states) with current log(Z) **********//
-        for(unsigned int E = 0; E < Nener; ++E){
+    unsigned int accuracy;
+    do {
+        // computing log(density of states) with current log(Z)
+        for (unsigned int E = 0; E < Nener; ++E) {
             double logZaehler = Counts[E][0];
-            double logNenner  = -Energy[E]/Temperature[0]-logZ[0].first+logN[0];
+            double logNenner  = -Energy[E] / Temperature[0] - logZ[0].first + logN[0];
 
-            for(unsigned int T = 1; T < Ntemp; ++T){
+            for (unsigned int T = 1; T < Ntemp; ++T) {
                 logZaehler += Counts[E][T];
-                logNenner = logplus(logNenner, -Energy[E]/Temperature[T]-logZ[T].first+logN[T]);
+                logNenner = logplus(logNenner, -Energy[E] / Temperature[T] - logZ[T].first + logN[T]);
             }
 
             logZaehler = log(logZaehler);
-            logOmega[E] = {Energy[E], logZaehler-logNenner};
+            logOmega[E] = {Energy[E], logZaehler - logNenner};
         }
 
-
-        //************** Computing next log(Z) with current log(dos) ***************//
-        for(unsigned int T = 0; T < Ntemp; ++T){
+        // computing next log(Z) with current log(dos
+        for (unsigned int T = 0; T < Ntemp; ++T) {
             logZ[T].second = logZ[T].first;
-            logZ[T].first = logOmega[0].second - logOmega[0].first/Temperature[T];
+            logZ[T].first = logOmega[0].second - logOmega[0].first / Temperature[T];
 
-            for(unsigned int E = 1; E < Nener; ++E){
-                logZ[T].first = logplus(logZ[T].first, logOmega[E].second - logOmega[E].first/Temperature[T]);
+            for (unsigned int E = 1; E < Nener; ++E) {
+                logZ[T].first = logplus(logZ[T].first,
+                                        logOmega[E].second - logOmega[E].first / Temperature[T]);
             }
         }
 
-
-        //*************************** Computing accuracy ***************************//
+        // computing accuracy
         accuracy = 0;
-        for(unsigned int T = 0; T < Ntemp; ++T ){
-            if(abs(logZ[T].first-logZ[T].second) < precision){++accuracy;}
+        for (unsigned int T = 0; T < Ntemp; ++T) {
+            if (abs(logZ[T].first - logZ[T].second) < precision) {++accuracy;}
         }
 
-    }while(accuracy < Ntemp);
+    } while (accuracy < Ntemp);
 
 
     delete[] logZ;
@@ -278,22 +295,23 @@ pair<double,double>* Multihistogram::compute_logDOS(double precision){
 }
 
 
-Multihistogram Multihistogram::operator-(const Multihistogram& input)const{
+Multihistogram Multihistogram::operator-(const Multihistogram& input)const
+{
     Multihistogram output = *this;
 
     vector<unsigned int> deleting;
-    for(unsigned int E1 = 0; E1 < output.Nener; ++E1){
-        for(unsigned int E2 = 0; E2 < input.Nener; ++E2){
+    for (unsigned int E1 = 0; E1 < output.Nener; ++E1) {
+        for (unsigned int E2 = 0; E2 < input.Nener; ++E2) {
 
-            if(abs(output.Energy[E1]-input.Energy[E2])<1e-10){
+            if (abs(output.Energy[E1] - input.Energy[E2]) < 1e-10) {
 
                 unsigned int Nzero = 0;
-                for(unsigned int T = 0; T < Ntemp; ++T){
+                for (unsigned int T = 0; T < Ntemp; ++T) {
                     output.Counts[E1][T] -= input.Counts[E2][T];
-                    if(output.Counts[E1][T]==0){++Nzero;}
+                    if (output.Counts[E1][T] == 0) {++Nzero;}
                 }
 
-                if(Nzero==Ntemp){
+                if (Nzero == Ntemp) {
                     deleting.push_back(E1);
                 }
 
@@ -302,16 +320,16 @@ Multihistogram Multihistogram::operator-(const Multihistogram& input)const{
         }
     }
 
-    for(unsigned int index : deleting){
-        if(index == output.Nener-1){
+    for (unsigned int index : deleting) {
+        if (index == output.Nener - 1) {
             output.Energy.pop_back();
             output.Counts.pop_back();
-        }else{
-            output.Energy.erase(output.Energy.begin()+index);
-            output.Counts.erase(output.Counts.begin()+index);  
-        }   
+        } else {
+            output.Energy.erase(output.Energy.begin() + index);
+            output.Counts.erase(output.Counts.begin() + index);
+        }
     }
- 
+
     output.Nener = output.Energy.size();
 
     return output;
